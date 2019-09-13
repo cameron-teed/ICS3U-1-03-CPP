ac8f01b75958cffe615a3375571c79fa4fbd3a87
Ubuntu <ubuntu@ip-172-31-21-52.ec2.internal>
2019-09-13 00:06:26 +0000

    made it easier to understand

---
+++ hello_world3.cpp
@@ -0,0 +1,11 @@
+// Copyright(c) 2019 Cameron Teed All rights reserved.
+//
+// Created by: Cameron Teed
+// Created on: September 2019
+// This is the "Hello, World!" program, but with nice style.
+
+# include <iostream>
+
+int main()  {
+    std::cout <<"Hello, World!"<< std::endl;
+}
